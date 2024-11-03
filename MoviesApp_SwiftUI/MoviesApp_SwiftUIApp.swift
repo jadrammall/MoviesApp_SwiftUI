2d451c1333b237c73de65e7a79b80e7b5f92214c
jadrammall <jrammal12@gmail.com>
2024-11-03 20:21:25 +0200

    Initial Commit

---
+++ MoviesApp_SwiftUI/MoviesApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp_SwiftUIApp.swift
+//  MoviesApp_SwiftUI
+//
+//  Created by Jad Rammal on 03/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
